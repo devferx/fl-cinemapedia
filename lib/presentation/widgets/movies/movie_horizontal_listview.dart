acca74f41e425fda9fea0fe3ddde3f93ea28a6ec
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-07-13 19:38:10 -0400

    Add slide movie in MovieHorizontalListview

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,5 +1,7 @@
-import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MovieHorizontalListview extends StatelessWidget {
   final List<Movie> movies;
@@ -23,6 +25,16 @@ class MovieHorizontalListview extends StatelessWidget {
         children: [
           if (title != null || subtitle != null)
             _Title(title: title, subtitle: subtitle),
+          Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return _Slide(movie: movies[index]);
+              },
+            ),
+          )
         ],
       ),
     );
@@ -54,5 +66,70 @@ class _Title extends StatelessWidget {
               ),
           ],
         ));
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const Center(
+                        child: CircularProgressIndicator(strokeWidth: 2));
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: textStyles.titleSmall,
+            ),
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star_half_outlined,
+                color: Colors.yellow.shade800,
+              ),
+              const SizedBox(width: 3),
+              Text(
+                '${movie.voteAverage}',
+                style: textStyles.bodyMedium?.copyWith(
+                  color: Colors.yellow.shade800,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Text('${movie.popularity}', style: textStyles.bodySmall)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
