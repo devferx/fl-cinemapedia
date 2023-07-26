50357b92932056b45852513e56b8914a6788a086
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-07-26 11:23:22 -0400

    Add Actors by movie as indepent widget

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -112,7 +112,7 @@ class _MovieDetails extends ConsumerWidget {
             ],
           ),
         ),
-        _ActorsByMovie(movieId: movie.id.toString()),
+        ActorsByMovie(movieId: movie.id.toString()),
         if (recomendedMovies != null && recomendedMovies.isNotEmpty)
           MovieHorizontalListview(
             title: "Recomendaciones",
@@ -120,65 +120,6 @@ class _MovieDetails extends ConsumerWidget {
           ),
         const SizedBox(height: 50),
       ],
-    );
-  }
-}
-
-class _ActorsByMovie extends ConsumerWidget {
-  final String movieId;
-
-  const _ActorsByMovie({required this.movieId});
-
-  @override
-  Widget build(BuildContext context, ref) {
-    final actorsByMovie = ref.watch(actorsByMovieProvider);
-    if (actorsByMovie[movieId] == null) {
-      return const CircularProgressIndicator(strokeWidth: 2);
-    }
-
-    final actors = actorsByMovie[movieId]!;
-
-    return SizedBox(
-      height: 300,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: actors.length,
-        itemBuilder: (context, index) {
-          final actor = actors[index];
-
-          return Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
-            width: 135,
-            child: FadeInRight(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: FadeInImage(
-                      height: 180,
-                      width: 135,
-                      fit: BoxFit.cover,
-                      placeholder: const AssetImage("assets/bottle-loader.gif"),
-                      image: NetworkImage(actor.profilePath),
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(actor.name, maxLines: 2),
-                  Text(
-                    actor.character ?? '',
-                    maxLines: 2,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
---
+++ lib/presentation/widgets/actors/actors_by_movie.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:animate_do/animate_do.dart';
+
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+class ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+
+  const ActorsByMovie({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+            width: 135,
+            child: FadeInRight(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: FadeInImage(
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                      placeholder: const AssetImage("assets/bottle-loader.gif"),
+                      image: NetworkImage(actor.profilePath),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(actor.name, maxLines: 2),
+                  Text(
+                    actor.character ?? '',
+                    maxLines: 2,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
