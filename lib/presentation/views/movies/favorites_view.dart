df60d8ecbd5ac4e8c2d8f068aa0a06eb84848f72
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-07-26 10:42:09 -0400

    Delete unnecessary import

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
