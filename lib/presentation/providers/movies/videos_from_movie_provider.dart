3398aa84d211a354983beae4a5f01ef9dff7bc21
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-07-27 11:59:17 -0400

    Add videos from movie provider

---
+++ lib/presentation/providers/movies/videos_from_movie_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/domain/entities/entities.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+final FutureProviderFamily<List<Video>, int> videosFromMovieProvider =
+    FutureProvider.family((ref, int movieId) {
+  final movieRepository = ref.watch(movieRepositoryProvider);
+  return movieRepository.getYoutubeVideosById(movieId);
+});
