500923279373bd40686c73b129413ebf2045bbec
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-07-27 11:34:38 -0400

    Add video entity

---
+++ lib/domain/entities/video.dart
@@ -0,0 +1,11 @@
+class Video {
+  final String id;
+  final String name;
+  final DateTime publishedAt;
+
+  Video({
+    required this.id,
+    required this.name,
+    required this.publishedAt,
+  });
+}
