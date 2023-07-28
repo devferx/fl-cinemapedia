753a3b42ef139631dfea56724b28f524fa6494a6
Fernando Quinteros Gutierrez <37563836+devferx@users.noreply.github.com>
2023-07-28 11:42:33 -0400

    Add popular movies provider in popular view

--- lib/presentation/views/movies/popular_view.dart
+++ lib/presentation/views/movies/popular_view.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
-class PopularView extends StatelessWidget {
+class PopularView extends ConsumerStatefulWidget {
   const PopularView({super.key});
 
   @override
+  PopularViewState createState() => PopularViewState();
+}
+
+class PopularViewState extends ConsumerState<PopularView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  void dispose() {
+    ref.read(popularMoviesProvider.notifier).dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final popularMovies = ref.watch(popularMoviesProvider);
+
+    if (popularMovies.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(strokeWidth: 2),
+      );
+    }
+
+    return Scaffold(
       body: MovieMasonry(
-        // loadNextPage: loadNextPage,
-        movies: [],
+        movies: popularMovies,
+        loadNextPage: () =>
+            ref.read(popularMoviesProvider.notifier).loadNextPage(),
       ),
     );
   }
